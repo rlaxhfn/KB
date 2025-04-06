a8feee1d34a17ae429e52f2e2aeaf221d021add3
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-06 15:39:11 +0900

    Docs: 10 VUE 심화 추가

---
+++ Frontend/Vue.js/10_VUE/심화/axios-test-app/src/App4.vue
@@ -0,0 +1,13 @@
+<template></template>
+<script setup>
+import axios from "axios";
+
+// 데이터 추가
+const requestAPI = async () => {
+  const listUrl = "/api/todos";
+  let data = { todo: "윗몸일으키기 3세트", desc: "너무 빠르지 않게..." }; // 추가할 데이터
+  const response = await axios.post(listUrl, data);
+  console.log(response.data);
+};
+requestAPI();
+</script>
---
+++ Frontend/Vue.js/10_VUE/심화/axios-test-app/src/App6.vue
@@ -0,0 +1,21 @@
+<template></template>
+<script setup>
+import axios from "axios";
+
+// 에러 처리
+const requestAPI = async () => {
+  const url = "/api/todos2";
+  axios
+    .get(url, { timeout: 900 }) // 실패 테스트
+    .then((response) => {
+      console.log("# 응답객체 : ", response);
+    })
+    .catch((e) => {
+      console.log("에러 발생");
+      console.log(e);
+      if (e instanceof Error) console.log(e.message);
+      else console.log(e);
+    }); // error catch
+};
+requestAPI();
+</script>
