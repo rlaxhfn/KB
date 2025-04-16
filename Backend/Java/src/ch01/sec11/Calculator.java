eb8e920e68cb84bb1c528df55c776bcb8289e16d
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-17 08:50:14 +0900

    Docs: 자바 시작하기 기본 문제

---
+++ Backend/Java/src/ch01/sec11/Calculator.java
@@ -0,0 +1,9 @@
+package ch01.sec11;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 1, y = 2;
+        int result = x + y;
+        System.out.println(result);
+    }
+}
