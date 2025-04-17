2081f0090900a27d9bc877a17c76e2cd5264148f
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-17 11:09:55 +0900

    Docs: 02 JAVA 심화 문제 추가

--- Backend/Java/02_JAVA/심화/src/ch04/Example.java
+++ Backend/Java/02_JAVA/심화/src/ch04/Example.java
@@ -15,31 +15,29 @@ public class Example {
             int choice = sc.nextInt();
             switch(choice) {
                 case 1:
-                    System.out.print("예금액>");
+                    System.out.print("예금액> ");
                     int moneyIn = sc.nextInt();
-                    System.out.println();
                     balance += moneyIn;
                     break;
                 case 2:
-                   System.out.print("출금액>");
+                   System.out.print("출금액> ");
                    int moneyOut = sc.nextInt();
                    if(moneyOut > balance) {
                        System.out.println("잔고가 부족합니다.");
-                       run = false;
                        break;
                    }
                    System.out.println();
                    balance -= moneyOut;
                    break;
                 case 3:
-                    System.out.println("잔고>"+balance);
+                    System.out.println("잔고> "+balance);
                     break;
                 case 4:
                     run = false;
                     break;
             }
             System.out.println();
-            System.out.println("프로그램 종료");
         }
+        System.out.println("프로그램 종료");
     }
 }
