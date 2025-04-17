6b3699da2fd2781911429d0f068508e21ccba141
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-17 09:32:09 +0900

    Docs: 기존 Java 폴더 삭제

--- Backend/Java/src/ch01/sec11/Calculator.java
+++
@@ -1,9 +0,0 @@
-package ch01.sec11;
-
-public class Calculator {
-    public static void main(String[] args) {
-        int x = 1, y = 2;
-        int result = x + y;
-        System.out.println(result);
-    }
-}
