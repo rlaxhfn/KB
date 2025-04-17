625aa6ea6f7d93c779b5aeb39aebc56e8046c311
rlaxhfn <k048204@hanyang.ac.kr>
2025-04-17 10:35:04 +0900

    Docs: 01 JAVA 기본 문제 추가

--- Backend/Java/01_JAVA/기본/src/ch02/sec01/VariableInitializationExample.java
+++ Backend/Java/01_JAVA/기본/src/ch02/sec01/VariableInitializationExample.java
@@ -4,6 +4,6 @@ public class VariableInitializationExample {
     public static void main(String[] args) {
         int value;
         int result = value + 10; // value에 할당된 값이 없어 error
-        System.out.println(value, result);
+        System.out.println(result);
     }
 }
