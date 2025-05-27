49828f8fabd55cacdedb7703b4aaa152f443979e
rlaxhfn <k048204@hanyang.ac.kr>
2025-05-28 00:39:20 +0900

    Docs: 08 MySQL_Travel 기본 문제

---
+++ Backend/MySQL/08_MySQL/Travel/기본/travelapp/src/main/java/org/scoula/travel/domain/TravelVO.java
@@ -0,0 +1,19 @@
+package org.scoula.travel.domain;
+
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TravelVO {
+    private Long no;
+    private String district;
+    private String title;
+    private String description;
+    private String address;
+    private String phone;
+    private List<TravelImageVO> images;
+}
